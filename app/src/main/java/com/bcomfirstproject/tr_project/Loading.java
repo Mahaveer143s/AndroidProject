9627b85ed446d0ca27d553cfd35421971bd206df
Trproject <mahaveer621s@gmail.com>
2022-09-19 08:45:24 +0530

    Tr mobile application

---
+++ app/src/main/java/com/bcomfirstproject/tr_project/Loading.java
@@ -0,0 +1,28 @@
+package com.bcomfirstproject.tr_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        Intent home = new Intent(Loading.this,MainActivity.class);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(home);
+                finish();
+            }
+        },4000);
+
+
+
+    }
+}
